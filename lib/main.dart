5daab1ba9154c56580bcdb3d251785c12e9b725a
Darlington Chigumbu <dchigumbu07@gmail.com>
2021-08-15 22:50:03 +0200

    info added

--- lib/main.dart
+++ lib/main.dart
@@ -10,23 +10,62 @@ class MyId extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Colors.grey[800],
       appBar: AppBar(
         title: Text("My application"),
         centerTitle: true,
-        backgroundColor: Colors.red[700],
+        backgroundColor: Colors.grey[700],
 
       ),
 
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text("me"),
+            Text(
+                "NAME",
+              style: TextStyle(
+                color: Colors.grey[400],
+                letterSpacing:2.0,
 
+              ),
 
+            ),
+            // another way of putting space between widgets
+            SizedBox(height:20.0),
+            Text(
+              "DARLINGTON CHIGUMBU",
+              style: TextStyle(
+                color: Colors.amberAccent [200],
+                letterSpacing:2.0,
+                fontWeight: FontWeight.bold,
+                fontSize: 22.0,
+              ),
+
+            ),
+            SizedBox(height:30.0),
+            Text(
+              "GAME LEVEL ",
+              style: TextStyle(
+                color: Colors.grey[400],
+                letterSpacing:2.0,
+
+              ),
+
+            ),
+            // another way of putting space between widgets
+            SizedBox(height:20.0),
+            Text(
+              "8",
+              style: TextStyle(
+                color: Colors.amberAccent [200],
+                letterSpacing:2.0,
+                fontWeight: FontWeight.bold,
+                fontSize: 22.0,
+              ),
+
+            ),
           ],
 
         ),
